0776d65160e1713c3436180908298aba44bec351
QuatreMains <plutokor@gmail.com>
2020-11-18 18:53:24 +0900

    done delete function

---
+++ src/components/Nweet.js
@@ -0,0 +1,71 @@
+import {React, useState} from "react";
+import { dbService, storageService } from "fBase";
+
+const Nweet = ({nweetObj, isOwner}) => {
+    const [editing, setEditing] = useState(false);
+    const [newNweet, setNewNweet] = useState(nweetObj.text);
+
+    const onDeleteClick = async () => {
+        const ok = window.confirm("Are you sure you want to delete this nweet?");
+        if (ok) {
+            await dbService.doc(`nweets/${nweetObj.id}`).delete();
+            await storageService.refFromURL(nweetObj.attachmentUrl).delete();
+        };
+    };
+
+    const onChange = (event) => {
+        const {
+            target: { value },
+        } = event;
+        setNewNweet(value);
+    };
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        await dbService.doc(`nweets/${nweetObj.id}`).update({
+            text:newNweet,
+        });
+        setEditing(false);
+    }
+
+    const toggleEditing = () => setEditing(prev => !prev);
+    return (
+        <div>
+            {
+                editing ? (
+                <>
+                    <form onSubmit={onSubmit}>
+                        <input 
+                            type="text"
+                            placeholder="Edit your nweet"
+                            value={newNweet}
+                            onChange={onChange}
+                            required
+                        />
+                        <input
+                            type="submit"
+                            value="Update Nweet"
+                        />
+                    </form>
+                    <button onClick={toggleEditing}>Cancel</button>
+                </>
+                ) : (
+                <>
+                <h4>{nweetObj.text}</h4>
+                    {nweetObj.attachmentUrl && (
+                        <img src={nweetObj.attachmentUrl} width="50px" height="50px" /> 
+                    )}
+                    {isOwner && (
+                        <>
+                            <button onClick={onDeleteClick}>Delete Nweet</button>
+                            <button onClick={toggleEditing}>Edit Nweet</button>
+                        </>
+                    )}
+                </> 
+                )
+            }
+        </div>
+    )
+}
+
+export default Nweet;
