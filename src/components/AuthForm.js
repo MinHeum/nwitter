83fffebcc3f6c1d9c9c6c905ca553a12a574bb56
QuatreMains <plutokor@gmail.com>
2020-12-16 16:12:05 +0900

    Added Styles

---
+++ src/components/AuthForm.js
@@ -0,0 +1,74 @@
+import { authService } from "fBase";
+import React, {useState} from "react";
+
+const AuthForm = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
+    const [error, setError] = useState("");
+
+    const onChange = (event) => {
+        const {target: {name, value},} =event   ;
+        if(name === "email") {
+            setEmail(value);
+        }
+        else if(name === "password") {
+            setPassword(value);
+        }
+    };
+    
+    const onSubmit = async (event) => {
+        event.preventDefault(); // 기본 행위가 실행되는것을 방지
+        try{
+            if (newAccount) {
+                //create Account
+                await authService.createUserWithEmailAndPassword(email, password);
+            }
+            else {
+                //log in
+                await authService.signInWithEmailAndPassword(email, password);
+            }
+        }
+        catch(error){
+            setError(error.message)
+        };
+    };
+
+    const toggleAccount = () => setNewAccount(prev => !prev);
+
+    return (
+        <>
+        <form onSubmit={onSubmit} className="container" >
+            <input
+                name="email"
+                type="email"
+                placeholder="Email"
+                required
+                value={email}
+                onChange={onChange}
+                className="authInput"
+            />
+            <input 
+                name="password"
+                type="password"
+                placeholder="Password"
+                required
+                value={password}
+                onChange={onChange}
+                className="authInput"
+            />
+            <input 
+                type="submit" 
+                value={newAccount ? "Create Account" : "Log In"}
+            />
+            {error && <span className="authError">{error}</span>}
+        </form>
+        <span onClick={toggleAccount} className="authSwitch">
+            {newAccount ? "Log in" : "Create Account" }
+        </span>
+        </>
+    )
+
+}
+
+export default AuthForm;
